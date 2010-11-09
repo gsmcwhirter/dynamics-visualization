06ed5ef17848f48f348aa73d00de8453f081e5b8
Gregory McWhirter <greg@hallofkvasir.org>
2010-11-09 00:21:09 -0800

    Able to draw BR graphs (probably)

--- applet/src/DynVizGraph.java
+++ applet/src/DynVizGraph.java
@@ -93,6 +93,11 @@ public class DynVizGraph extends JApplet {
             payoffD = 2;
         }
 
+        System.out.println(payoffA);
+        System.out.println(payoffB);
+        System.out.println(payoffC);
+        System.out.println(payoffD);
+
         JPanel panel = new JPanel(new GridLayout(1,3));
 
         BRChart = new CanvasPanel();
@@ -115,10 +120,11 @@ public class DynVizGraph extends JApplet {
         BRGen.start();
     }
     
-    private void BRGraphInfo(int[] lines){
+    private void BRGraphInfo(Line[] lines, Line[] arrows){
         //BRChart
         BRChart.setLines(lines);
-        System.out.println("The test object is null");
+        BRChart.setArrows(arrows);
+        System.out.println("Set lines");
         //BRChart.repaint();
     }
     
@@ -128,6 +134,59 @@ public class DynVizGraph extends JApplet {
     
     private void CtRGraphInfo(BufferedImage bi){
         
+    }
+
+    class Line {
+        private int[] coords;
+        private Color color;
+        private Color color2;
+
+        public Line(int c1, int c2, int c3, int c4, Color c, Color cp){
+            coords = new int[4];
+            coords[0] = c1;
+            coords[1] = c2;
+            coords[2] = c3;
+            coords[3] = c4;
+            color = c;
+            color2 = cp;
+        }
+
+        public Line(int c1, int c2, int c3, int c4, Color c){
+            coords = new int[4];
+            coords[0] = c1;
+            coords[1] = c2;
+            coords[2] = c3;
+            coords[3] = c4;
+            color = c;
+            color2 = Color.BLACK;
+        }
+
+        public Line(int c1, int c2, int c3, int c4){
+            coords = new int[4];
+            coords[0] = c1;
+            coords[1] = c2;
+            coords[2] = c3;
+            coords[3] = c4;
+            color = Color.BLACK;
+            color2 = Color.BLACK;
+        }
+
+        public Color getColor(){
+            return color;
+        }
+
+        public Color getColor2(){
+            return color2;
+        }
+
+        public int[] getCoords(){
+            return coords;
+        }
+
+        @Override
+        public String toString(){
+            return coords.toString();
+        }
     }
 
     class BRGraphGenerator implements Runnable {
@@ -142,61 +201,174 @@ public class DynVizGraph extends JApplet {
 
         @Override
         public void run(){
-            BufferedImage bi = new BufferedImage(200, 200, BufferedImage.TYPE_INT_ARGB);
-            Graphics2D g2d = bi.createGraphics();
+            Line[] lines;
+            Line[] arrows;
 
             //draw stuff
-            int qlim = 200 * (1 - (int) Math.floor(C / (A + C)));
+            float qlimf = (float)C / (float)(A + C);
+            System.out.println(qlimf);
+            int qlim = (int) Math.floor(200f * (1f - qlimf));
             System.out.println(qlim);
 
-            g2d.setColor(Color.RED);
+            lines = new Line[106];
+            int index = 0;
+            int index2 = 0;
+            float brlimx;
+            float brlimy;
+            int lrespx;
+            int lrespy;
+
             if (A + C > 0){
-                g2d.drawLine(200, qlim, 200, 0);
-                g2d.drawLine(0, qlim, 0, 200);
-                g2d.drawLine(0, qlim, 200, qlim);
+                lrespx = 0;
+                if (qlimf <= 1f && qlimf >= 0f){
+                    lines[index++] = new Line(200, qlim, 200, 0, Color.RED);
+                    lines[index++] = new Line(0, qlim, 0, 200, Color.RED);
+                    lines[index++] = new Line(0, qlim, 200, qlim, Color.RED);
+                    brlimy = qlimf;
+                } else if (qlimf > 1f) {
+                    //always play the other
+                    lines[index++] = new Line(0, 0, 0, 200, Color.RED);
+                    brlimy = 1f;
+                } else {
+                    //always play this
+                    lines[index++] = new Line(200, 0, 200, 200, Color.RED);
+                    brlimy = 0f;
+                }
             } else if (A + C < 0){
-                g2d.drawLine(200, qlim, 200, 200);
-                g2d.drawLine(0, qlim, 0, 0);
-                g2d.drawLine(0, qlim, 200, qlim);
+                lrespx = 200;
+                if (qlimf <= 1f && qlimf >= 0f){
+                    lines[index++] = new Line(200, qlim, 200, 200, Color.RED);
+                    lines[index++] = new Line(0, qlim, 0, 0, Color.RED);
+                    lines[index++] = new Line(0, qlim, 200, qlim, Color.RED);
+                    brlimy = qlimf;
+                } else if (qlimf > 1f) {
+                    //always play this
+                    lines[index++] = new Line(200, 0, 200, 200, Color.RED);
+                    brlimy = 0f;
+                } else {
+                    //always play the other
+                    lines[index++] = new Line(0, 0, 0, 200, Color.RED);
+                    brlimy = 1f;
+                }
             } else if (0 >= C) {
-                g2d.drawLine(200, 0, 200, 200);
+                //play this
+                lines[index++] = new Line(200, 0, 200, 200, Color.RED);
+                lrespx = 0;
+                brlimy = 0f;
             } else {
-                g2d.drawLine(0, 0, 0, 200);
+                //play other
+                lines[index++] = new Line(0, 0, 0, 200, Color.RED);
+                lrespx = 0;
+                brlimy = 1f;
             }
 
-            g2d.setColor(Color.BLUE);
+            float plimf = (float)D / (float)(B + D);
+            int plim = (int) Math.floor(200f * (1 - plimf));
+
             if (B + D > 0){
-                g2d.drawLine(qlim, 200, 0, 200);
-                g2d.drawLine(qlim, 0, 200, 0);
-                g2d.drawLine(qlim, 0, qlim, 200);
+                lrespy = 0;
+                if (plimf >= 0f && plimf <= 1f){
+                    lines[index++] = new Line(plim, 200, 0, 200, Color.BLUE);
+                    lines[index++] = new Line(plim, 0, 200, 0, Color.BLUE);
+                    lines[index++] = new Line(plim, 0, plim, 200, Color.BLUE);
+                    brlimx = plimf;
+                } else if (plimf > 1f){
+                    //play the other
+                    lines[index++] = new Line(0, 200, 200, 0, Color.BLUE);
+                    brlimx = 1f;
+                } else {
+                    //play this
+                    lines[index++] = new Line(0, 200, 200, 200, Color.BLUE);
+                    brlimx = 0f;
+                }
             } else if (B + D < 0){
-                g2d.drawLine(qlim, 200, 200, 200);
-                g2d.drawLine(qlim, 0, 0, 0);
-                g2d.drawLine(qlim, 0, qlim, 200);
+                lrespy = 200;
+                if (plimf >= 0f && plimf <= 1f){
+                    lines[index++] = new Line(plim, 200, 200, 200, Color.BLUE);
+                    lines[index++] = new Line(plim, 0, 0, 0, Color.BLUE);
+                    lines[index++] = new Line(plim, 0, plim, 200, Color.BLUE);
+                    brlimx = plimf;
+                } else if (plimf > 1f) {
+                    //play this
+                    lines[index++] = new Line(0, 200, 200, 200, Color.BLUE);
+                    brlimx = 0f;
+                } else {
+                    //play the other
+                    lines[index++] = new Line(0, 200, 200, 0, Color.BLUE);
+                    brlimx = 1f;
+                }
             } else if (0 >= D) {
-                g2d.drawLine(0, 200, 200, 200);
+                //play this
+                lines[index++] = new Line(0, 200, 200, 200, Color.BLUE);
+                lrespy = 0;
+                brlimx = 0f;
             } else {
-                g2d.drawLine(0, 0, 200, 0);
+                //play other
+                lines[index++] = new Line(0, 200, 200, 0, Color.BLUE);
+                lrespy = 0;
+                brlimx = 1f;
             }
 
-            BRGraphInfo(bi);
+            arrows = new Line[100];
+            float step = 0.1f;
+            int fmx;
+            int fmy;
+            int tox;
+            int toy;
+            for (float i = 0f; i < 1f; i += step){
+                for (float j = 0f; j < 1f; j += step){
+                    //response at (i, j)
+                    fmx = (int) Math.floor( 200 * (1f - i) );
+                    fmy = (int) Math.floor( 200 * (1f - j) );
+
+                    if (j < brlimy){
+                        tox = lrespx;
+                    } else if (j > brlimy) {
+                        tox = Math.abs(lrespx - 200);
+                    } else {
+                        tox = 100;
+                    }
+
+                    if (i < brlimx){
+                        toy = lrespy;
+                    } else if (i > brlimx) {
+                        toy = Math.abs(lrespy - 200);
+                    } else {
+                        toy = 100;
+                    }
+
+                    arrows[index2++] = new Line(fmx, fmy, tox, toy, Color.GREEN, Color.BLACK);
+                    lines[index++] = new Line(fmx, fmy, fmx, fmy, Color.BLACK);
+                }
+            }
+
+            BRGraphInfo(lines, arrows);
         }
     }
 
     // Class to provide a drawing surface for graphics display
     class CanvasPanel extends JPanel {
+        static final int BR = 1;
+        static final int DtR = 2;
+        static final int CtR = 3;
 
-        private int[] _lines;
+        private Line[] _lines;
+        private Line[] _arrows;
+        private int _typ;
 
         // Constructor
         public CanvasPanel(){
-           setBackground(Color.white);
-           setPreferredSize( new Dimension( chartWidth, chartHeight ) );  // Set its size
-           setVisible( true );   // Make the window visible
+            setBackground(Color.white);
+            setPreferredSize( new Dimension( chartWidth, chartHeight ) );  // Set its size
+            setVisible( true );   // Make the window visible
         }
 
-        public void setLines(int[] lines){
+        public void setLines(Line[] lines){
             _lines = lines;
+        }
+
+        public void setArrows(Line[] arrows){
+            _arrows = arrows;
         }
 
         @Override
@@ -205,14 +377,93 @@ public class DynVizGraph extends JApplet {
 
             Graphics2D g2d = (Graphics2D) g;
 
+            g2d.setColor(Color.LIGHT_GRAY);
             g2d.drawRect(5, 5, 200, 200);
 
-            if (_lines.length > 0) {
-                g2d.drawLine(5, 205, 205, 5);
+            Color color;
+            Color color2;
+            int[] coords;
 
-                
+            if (_arrows != null && _arrows.length > 0) {
+                for (int i = 0; i < _arrows.length; i++){
+                    if (_arrows[i] != null){
+                        coords = _arrows[i].getCoords();
+                        color = _arrows[i].getColor();
+                        color2 = _arrows[i].getColor2();
+
+                        drawArrow(g2d, coords[0] + 5, coords[1] + 5, coords[2] + 5, coords[3] + 5, color, color2);
+                    }
+                }
             }
 
+            if (_lines != null && _lines.length > 0) {
+                System.out.println("We have lines!");
+
+                for (int i = 0; i < _lines.length; i++){
+                    if (_lines[i] != null){
+                        coords = _lines[i].getCoords();
+                        color = _lines[i].getColor();
+                        g2d.setColor(color);
+                        g2d.drawLine(coords[0] + 5, coords[1] + 5, coords[2] + 5, coords[3] + 5);
+                    }
+                }
+            }
+
+
+        }
+
+        /**
+         * Draws an arrow on the given Graphics2D context
+         * @param g The Graphics2D context to draw on
+         * @param x The x location of the "tail" of the arrow
+         * @param y The y location of the "tail" of the arrow
+         * @param xx The x location of the "head" of the arrow
+         * @param yy The y location of the "head" of the arrow
+         */
+        private void drawArrow( Graphics2D g, int x, int y, int xx, int yy, Color lcolor, Color acolor )
+        {
+            float arrowWidth = 5.0f ;
+            float theta = 0.423f ;
+            int[] xPoints = new int[ 3 ] ;
+            int[] yPoints = new int[ 3 ] ;
+            float[] vecLine = new float[ 2 ] ;
+            float[] vecLeft = new float[ 2 ] ;
+            float fLength;
+            float th;
+            float ta;
+            float baseX, baseY ;
+
+            xPoints[ 0 ] = xx ;
+            yPoints[ 0 ] = yy ;
+
+            // build the line vector
+            vecLine[ 0 ] = (float)xPoints[ 0 ] - x ;
+            vecLine[ 1 ] = (float)yPoints[ 0 ] - y ;
+
+            // build the arrow base vector - normal to the line
+            vecLeft[ 0 ] = -vecLine[ 1 ] ;
+            vecLeft[ 1 ] = vecLine[ 0 ] ;
+
+            // setup length parameters
+            fLength = (float)Math.sqrt( vecLine[0] * vecLine[0] + vecLine[1] * vecLine[1] ) ;
+            th = arrowWidth / ( 2.0f * fLength ) ;
+            ta = arrowWidth / ( 2.0f * ( (float)Math.tan( theta ) / 2.0f ) * fLength ) ;
+
+            // find the base of the arrow
+            baseX = ( (float)xPoints[ 0 ] - ta * vecLine[0]);
+            baseY = ( (float)yPoints[ 0 ] - ta * vecLine[1]);
+
+            // build the points on the sides of the arrow
+            xPoints[ 1 ] = (int)( baseX + th * vecLeft[0] );
+            yPoints[ 1 ] = (int)( baseY + th * vecLeft[1] );
+            xPoints[ 2 ] = (int)( baseX - th * vecLeft[0] );
+            yPoints[ 2 ] = (int)( baseY - th * vecLeft[1] );
+
+            g.setColor(lcolor);
+            g.drawLine( x, y, (int)baseX, (int)baseY ) ;
+
+            g.setColor(acolor);
+            g.fillPolygon( xPoints, yPoints, 3 ) ;
         }
      } // End canvasPanel class
 
