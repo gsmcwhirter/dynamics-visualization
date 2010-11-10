3b565329b29b5121c59a485063021eeefdcf8044
Greg McWhirter <greg@hallofkvasir.org>
2010-11-10 15:37:13 -0800

    IE can burn... This is ridiculous

--- client/js/app.js
+++ client/js/app.js
@@ -654,8 +654,7 @@ var app = $.sammy("#container2", function (){
                     ["%param", {name: "A", value: game['tl-r'] - game['bl-r']}],
                     ["%param", {name: "B", value: game['tl-c'] - game['tr-c']}],
                     ["%param", {name: "C", value: game['br-r'] - game['tr-r']}],
-                    ["%param", {name: "D", value: game['br-c'] - game['bl-c']}],
-                    ["%span", "Applet failed to run. No Java plug-in was found."]
+                    ["%param", {name: "D", value: game['br-c'] - game['bl-c']}]
                 ]]
             );
 
