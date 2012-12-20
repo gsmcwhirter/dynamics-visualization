ecf307a2b501feaeba1eafc4b216ae44a4d9713e
Gregory McWhirter <greg@hallofkvasir.org>
2012-12-20 11:22:30 -0800

    Finished the input bindings. Just need to generate actual graphs now.

--- game.js
+++ game.js
@@ -9,36 +9,410 @@ var game_types = {
 , 'zsum': "Asymmetric, Zero-Sum"
 };
 
-module.exports = {
-  is_symmetric: is_symmetric
-, identify: identify_game
-, is_battleofthesexes: is_battleofthesexes
-, is_matchingpennies: is_matchingpennies
-, is_zerosum: is_zerosum
-, get_type: get_game_type
+var event = require("event")
+  , dom = require("dom")
+  , each = require("each")
+  , storage = require("storage")
+  , emitter = require("emitter")
+  , indexOf = require("indexof")
+  
+  , game_tmpl = require('./templates/game')
+  , game_store = storage('games')
+  ;
+  
+module.exports = Game;
+
+function Game(game){
+  this.game = game || null;
+  
+  if (game){
+    this.dom = dom(game_tmpl({startindex: 0, game: game}));
+    this.setDisplayValues();
+    this.setDisplayHeaders();
+  }
+  else {
+    this.dom = dom(game_tmpl());
+  }
+  
+  this._symmetric_set = false;
+  this._zerosum_set = false;
+  this._autofill_set = false;
+  this.setBindings();
+}
+
+emitter(Game.prototype);
+
+Game.prototype.toggleInput = function (off, cr){
+  var tohide = ['input.entry-input[name=tl-' + cr + ']', 'input.entry-input[name=tr-' + cr + ']', 
+                'input.entry-input[name=bl-' + cr + ']', 'input.entry-input[name=br-' + cr + ']']
+    , toshow = ['.entry.tl-' + cr, '.entry.tr-' + cr, '.entry.bl-' + cr, '.entry.br-' + cr]
+    , self = this
+    ;
+  
+  function hide(tohide){
+    each(tohide, function (sel){
+      self.dom.find(sel).css('display', 'none');
+    });
+  }
+  
+  function show(toshow){
+    each(toshow, function (sel){
+      self.dom.find(sel).css('display', 'inline');
+    });
+  }
+  
+  if (off){
+    hide(tohide);
+    show(toshow);
+  }
+  else {
+    hide(toshow);
+    show(tohide);
+  }
+};
+
+Game.prototype.toggleColumnInput = function (off){
+  this.toggleInput(off, 'c');
+};
+
+Game.prototype.toggleRowInput = function (off){
+  this.toggleInput(off, 'r');
+}
+
+Game.prototype.setSymmetricBindings = function (symmetric){
+  var self = this;
+  var newels = {
+    'tl': 'tl'
+  , 'tr': 'bl'
+  , 'bl': 'tr'
+  , 'br': 'br'
+  }
+  
+  function input_binding(el){
+    var newel = newels[el];
+    
+    return function (){
+      var val = parseInt(self.dom.find('input.entry-input[name=' + el + '-r]').els[0].value) || 0;
+      self.dom.find('input.entry-input[name=' + newel + '-c]').els[0].value = val;
+      self.dom.find('.entry.' + newel + '-c').els[0].innerHTML = val;
+    }
+  }
+    
+  if (symmetric && !this._symmetric_set){
+    this._symmetric_set = true;
+    
+    this.toggleColumnInput(true);
+    
+    each(['tl', 'tr', 'bl', 'br'], function (el){
+      var domel = self.dom.find('input.entry-input[name=' + el + '-r]').els[0]; 
+      event.bind(domel, 'change', input_binding(el));
+      event.bind(domel, 'blur', input_binding(el));
+      event.bind(domel, 'keypress', input_binding(el));
+      domel.blur()
+    });
+    
+  }
+  else if (!symmetric && this._symmetric_set){
+    this._symmetric_set = false;
+    
+    this.toggleColumnInput(false);
+    
+    each(['tl', 'tr', 'bl', 'br'], function (el){
+      var domel = self.dom.find('input.entry-input[name=' + el + '-r]').els[0]; 
+      event.unbind(domel, 'change', input_binding(el));
+      event.unbind(domel, 'blur', input_binding(el));
+      event.unbind(domel, 'keypress', input_binding(el));
+    });
+  }
+};
+
+Game.prototype.setZeroSumBindings = function (zerosum){
+  var self = this;
+
+  function input_binding(el){
+    return function (){
+      var val = parseInt(self.dom.find('input.entry-input[name=' + el + '-r]').els[0].value) || 0;
+      self.dom.find('input.entry-input[name=' + el + '-c]').els[0].value = -1 * val;
+      self.dom.find('.entry.' + el + '-c').els[0].innerHTML = (-1 * val).toString();
+    }
+  }
+
+  if (zerosum && !this._zerosum_set){
+    this._zerosum_set = true;
+    
+    this.toggleColumnInput(true);
+    
+    each(['tl', 'tr', 'bl', 'br'], function (el){
+      var domel = self.dom.find('input.entry-input[name=' + el + '-r]').els[0]; 
+      event.bind(domel, 'change', input_binding(el));
+      event.bind(domel, 'blur', input_binding(el));
+      event.bind(domel, 'keypress', input_binding(el));
+    });
+  }
+  else if (!zerosum && this._zerosum_set){
+    this._zerosum_set = false;
+    
+    this.toggleColumnInput(false);
+    
+    each(['tl', 'tr', 'bl', 'br'], function (el){
+      var domel = self.dom.find('input.entry-input[name=' + el + '-r]').els[0]; 
+      event.unbind(domel, 'change', input_binding(el));
+      event.unbind(domel, 'blur', input_binding(el));
+      event.unbind(domel, 'keypress', input_binding(el));
+    });
+  }
+};
+
+Game.prototype.setAutofillBindings = function (autofill){
+  if (autofill){
+    this._autofill_set = true;
+    
+    this.toggleRowInput(true);
+    this.toggleColumnInput(true);
+    
+    var aftype = this.dom.find('select[name=presets]').els[0].value;
+    var newgame = false;
+    
+    //['sh', 'pd', 'hd', 'pl', 'mp', 'bos']
+    switch (aftype){
+      case 'sh':
+        newgame = {
+          'tl-r': 4
+        , 'tl-c': 4
+        , 'tr-r': 0
+        , 'tr-c': 3
+        , 'bl-r': 3
+        , 'bl-c': 0
+        , 'br-r': 2
+        , 'br-c': 2
+        };
+        break;
+      case 'pd':
+        newgame = {
+          'tl-r': 3
+        , 'tl-c': 3
+        , 'tr-r': 0
+        , 'tr-c': 4
+        , 'bl-r': 4
+        , 'bl-c': 0
+        , 'br-r': 1
+        , 'br-c': 1
+        };
+        break;
+      case 'hd':
+        newgame = {
+          'tl-r': 3
+        , 'tl-c': 3
+        , 'tr-r': 2
+        , 'tr-c': 4
+        , 'bl-r': 4
+        , 'bl-c': 2
+        , 'br-r': 0
+        , 'br-c': 0
+        };
+        break;
+      case 'pl':
+        newgame = {
+          'tl-r': 4
+        , 'tl-c': 4
+        , 'tr-r': 1
+        , 'tr-c': 3
+        , 'bl-r': 3
+        , 'bl-c': 1
+        , 'br-r': 0
+        , 'br-c': 0
+        };
+        break;
+      case 'mp':
+        newgame = {
+          'tl-r': 1
+        , 'tl-c': -1
+        , 'tr-r': -1
+        , 'tr-c': 1
+        , 'bl-r': -1
+        , 'bl-c': 1
+        , 'br-r': 1
+        , 'br-c': -1
+        };
+        break;
+      case 'bos':
+        newgame = {
+          'tl-r': 3
+        , 'tl-c': 2
+        , 'tr-r': 0
+        , 'tr-c': 0
+        , 'bl-r': 0
+        , 'bl-c': 0
+        , 'br-r': 2
+        , 'br-c': 3
+        };
+        break; 
+    }
+    
+    this.setInputValues(newgame);
+    this.setDisplayValues(newgame);
+  }
+  else if (!autofill && this._autofill_set){
+    this._autofill_set = false;
+    
+    this.toggleRowInput(false);
+    this.toggleColumnInput(false);
+  }
+}
+
+Game.prototype.changeBindings = function (){
+  var self = this
+    , sbox = this.dom.find('select[name=presets]').els[0]
+    , sboxval = sbox.options[sbox.selectedIndex].value
+    , symbind = (sboxval === 's')
+    , zsumbind = (sboxval === 'zsum')
+    , autobind = (indexOf(['sh', 'pd', 'hd', 'pl', 'mp', 'bos'], sboxval) !== -1)
+    , bindings = [symbind, zsumbind, autobind]
+    ;
+
+  each(bindings, function (tf, ind){
+    if (!tf){
+      switch (ind){
+        case 0:
+          self.setSymmetricBindings(tf);
+          break;
+        case 1:
+          self.setZeroSumBindings(tf);
+          break;
+        case 2:
+          self.setAutofillBindings(tf);
+          break;
+      }
+    }
+  });
+  
+  each(bindings, function (tf, ind){
+    if (tf){
+      switch (ind){
+        case 0:
+          self.setSymmetricBindings(tf);
+          break;
+        case 1:
+          self.setZeroSumBindings(tf);
+          break;
+        case 2:
+          self.setAutofillBindings(tf);
+          break;
+      }
+    }
+  });
+}
+
+Game.prototype.setBindings = function (){
+  var self = this;
+  
+  this.setSymmetricBindings(false);
+  this.setZeroSumBindings(false);
+  this.setAutofillBindings(false);
+  
+  event.bind(this.dom.find('a.save-button').els[0], 'click', function (){ self.stopEditing(); });
+  
+  event.bind(this.dom.find('a.process-button').els[0], 'click', function (){ self.generateGraphs(); });
+  
+  event.bind(this.dom.find('a.edit-button').els[0], 'click', function (){ self.startEditing(); });
+  
+  event.bind(this.dom.find('a.delete-game').els[0], 'click', function (){
+    document.getElementById('main').removeChild(self.dom.els[0]);
+    
+    self.game = null;
+    self.emit('change');
+  }); 
+  
+  var selectbox = this.dom.find('select[name=presets]').els[0];
+  event.bind(selectbox, 'change', function (){ self.changeBindings(); });
+  event.bind(selectbox, 'blur', function (){ self.changeBindings(); });  
+};
+
+Game.prototype.startEditing = function (){
+  this.dom.find("input[name=label]").els[0].value = this.game.label || "";
+  this.setInputValues();
+  
+  this.dom.first().addClass('editing');  
+};
+
+Game.prototype.stopEditing = function (){
+  this.game = this.game || {};
+  
+  this.game.label = this.dom.find("input[name=label]").els[0].value || "";
+  this.game['tl-r'] = parseInt(this.dom.find("input.entry-input[name=tl-r]").els[0].value || 0);
+  this.game['tl-c'] = parseInt(this.dom.find("input.entry-input[name=tl-c]").els[0].value || 0);
+  this.game['tr-r'] = parseInt(this.dom.find("input.entry-input[name=tr-r]").els[0].value || 0);
+  this.game['tr-c'] = parseInt(this.dom.find("input.entry-input[name=tr-c]").els[0].value || 0);
+  this.game['bl-r'] = parseInt(this.dom.find("input.entry-input[name=bl-r]").els[0].value || 0);
+  this.game['bl-c'] = parseInt(this.dom.find("input.entry-input[name=bl-c]").els[0].value || 0);
+  this.game['br-r'] = parseInt(this.dom.find("input.entry-input[name=br-r]").els[0].value || 0);
+  this.game['br-c'] = parseInt(this.dom.find("input.entry-input[name=br-c]").els[0].value || 0);
+  
+  this.setDisplayValues();
+  this.setDisplayHeaders();
+  
+  this.dom.first().removeClass('editing');
+  this.emit('change');
+};
+
+Game.prototype.setDisplayValues = function (game){
+  game = game || this.game;
+
+  this.dom.find(".entry.tl-r").els[0].innerHTML = game['tl-r'] || 0;
+  this.dom.find(".entry.tl-c").els[0].innerHTML = game['tl-c'] || 0;
+  this.dom.find(".entry.tr-r").els[0].innerHTML = game['tr-r'] || 0;
+  this.dom.find(".entry.tr-c").els[0].innerHTML = game['tr-c'] || 0;
+  this.dom.find(".entry.bl-r").els[0].innerHTML = game['bl-r'] || 0;
+  this.dom.find(".entry.bl-c").els[0].innerHTML = game['bl-c'] || 0;
+  this.dom.find(".entry.br-r").els[0].innerHTML = game['br-r'] || 0;
+  this.dom.find(".entry.br-c").els[0].innerHTML = game['br-c'] || 0; 
+};
+
+Game.prototype.setDisplayHeaders = function (){
+  this.dom.find("h1.label").els[0].innerHTML = this.game.label || "";
+  if (typeof this.game === "object"){
+    this.dom.find("h2.game-type").els[0].innerHTML = this.getGameType();
+  }
+};
+
+Game.prototype.setInputValues = function (game){
+  game = game || this.game || {};
+  
+  this.dom.find("input.entry-input[name=tl-r]").els[0].value = game['tl-r'] || 0;
+  this.dom.find("input.entry-input[name=tl-c]").els[0].value = game['tl-c'] || 0;
+  this.dom.find("input.entry-input[name=tr-r]").els[0].value = game['tr-r'] || 0;
+  this.dom.find("input.entry-input[name=tr-c]").els[0].value = game['tr-c'] || 0;
+  this.dom.find("input.entry-input[name=bl-r]").els[0].value = game['bl-r'] || 0;
+  this.dom.find("input.entry-input[name=bl-c]").els[0].value = game['bl-c'] || 0;
+  this.dom.find("input.entry-input[name=br-r]").els[0].value = game['br-r'] || 0;
+  this.dom.find("input.entry-input[name=br-c]").els[0].value = game['br-c'] || 0;
+};
+
+Game.prototype.generateGraphs = function (){
+  //TODO: implement
 };
 
 /**
  * Determines if the game is symmetric or not
  */
-function is_symmetric(game){
-  return  (game['tl-r'] || 0) == (game['tl-c'] || 0) &&
-          (game['tr-r'] || 0) == (game['bl-c'] || 0) &&
-          (game['bl-r'] || 0) == (game['tr-c'] || 0) &&
-          (game['br-r'] || 0) == (game['br-c'] || 0);
-}
+Game.prototype.isSymmetric = function (){
+  return  (this.game['tl-r'] || 0) == (this.game['tl-c'] || 0) &&
+          (this.game['tr-r'] || 0) == (this.game['bl-c'] || 0) &&
+          (this.game['bl-r'] || 0) == (this.game['tr-c'] || 0) &&
+          (this.game['br-r'] || 0) == (this.game['br-c'] || 0);
+};
 
 /**
  * Classifies the game
  *
  */
-function identify_game(game){
-  if (!is_symmetric(game)){
-    return is_battleofthesexes(game) || is_matchingpennies(game) || is_zerosum(game) || false;
+Game.prototype.identify = function (){
+  if (!this.isSymmetric()){
+    return this.isBattleOfTheSexes() || this.isMatchingPennies() || this.isZeroSum() || false;
   }
   
-  var a = (game['tl-r'] || 0) - (game['bl-r'] || 0);
-  var b = (game['br-r'] || 0) - (game['tr-r'] || 0);
+  var a = (this.game['tl-r'] || 0) - (this.game['bl-r'] || 0);
+  var b = (this.game['br-r'] || 0) - (this.game['tr-r'] || 0);
 
   if (a > 0 && b > 0) {
       return 'sh';
@@ -55,26 +429,26 @@ function identify_game(game){
   else {
       return 'edge';
   }
-}
+};
 
 /**
  * Determines if the game is a Battle of the Sexes
  *
  */
-function is_battleofthesexes(game){
-  var min = Math.min(game['tl-r'], game['tl-c'], game['tr-r'], game['tl-c'],
-          game['bl-r'], game['bl-c'], game['br-r'], game['br-c']);
+Game.prototype.isBattleOfTheSexes = function (){
+  var min = Math.min(this.game['tl-r'], this.game['tl-c'], this.game['tr-r'], this.game['tl-c'],
+          this.game['bl-r'], this.game['bl-c'], this.game['br-r'], this.game['br-c']);
 
   var game2 = {};
 
-  game2['tl-r'] = game['tl-r'] - min;
-  game2['tr-r'] = game['tr-r'] - min;
-  game2['bl-r'] = game['bl-r'] - min;
-  game2['br-r'] = game['br-r'] - min;
-  game2['tl-c'] = game['tl-c'] - min;
-  game2['tr-c'] = game['tr-c'] - min;
-  game2['bl-c'] = game['bl-c'] - min;
-  game2['br-c'] = game['br-c'] - min;
+  game2['tl-r'] = this.game['tl-r'] - min;
+  game2['tr-r'] = this.game['tr-r'] - min;
+  game2['bl-r'] = this.game['bl-r'] - min;
+  game2['br-r'] = this.game['br-r'] - min;
+  game2['tl-c'] = this.game['tl-c'] - min;
+  game2['tr-c'] = this.game['tr-c'] - min;
+  game2['bl-c'] = this.game['bl-c'] - min;
+  game2['br-c'] = this.game['br-c'] - min;
 
   if (Math.min(game2['tr-r'], game2['tr-c'], game2['bl-r'], game2['bl-c']) == 0 &&
       Math.max(game2['tr-r'], game2['tr-c'], game2['bl-r'], game2['bl-c']) == 0 &&
@@ -89,34 +463,34 @@ function is_battleofthesexes(game){
   }
 
   return false;
-}
+};
 
 /**
  * Determines if the game is a Matching Pennies
  *
  */
-function is_matchingpennies(game){
-  return  is_zerosum(game) &&
-          (game['tl-r'] + game['tr-r'] == 0) &&
-          (game['tl-r'] + game['bl-r'] == 0) &&
-          (game['bl-r'] + game['br-r'] == 0) ? 'mp' : false;
-}
+Game.prototype.isMatchingPennies = function (){
+  return  this.isZeroSum() &&
+          (this.game['tl-r'] + this.game['tr-r'] == 0) &&
+          (this.game['tl-r'] + this.game['bl-r'] == 0) &&
+          (this.game['bl-r'] + this.game['br-r'] == 0) ? 'mp' : false;
+};
 
 /**
  * Determines if the game is zero-sum
  *
  */
-function is_zerosum(game){
-  return  (game['tl-r'] + game['tl-c'] == 0) &&
-          (game['tr-r'] + game['tr-c'] == 0) &&
-          (game['bl-r'] + game['bl-c'] == 0) &&
-          (game['br-r'] + game['br-c'] == 0) ? 'zsum' : false;
-}
+Game.prototype.isZeroSum = function (){
+  return  (this.game['tl-r'] + this.game['tl-c'] == 0) &&
+          (this.game['tr-r'] + this.game['tr-c'] == 0) &&
+          (this.game['bl-r'] + this.game['bl-c'] == 0) &&
+          (this.game['br-r'] + this.game['br-c'] == 0) ? 'zsum' : false;
+};
 
 /**
  * Get the name of the type of the game
  *
  */
-function get_game_type(game){
-  return game_types[identify_game(game)] || "Asymmetric";
-}
+Game.prototype.getGameType = function (){
+  return game_types[this.identify()] || "Asymmetric";
+};
--- index.js
+++ index.js
@@ -1,24 +1,17 @@
 var page = require("page")
-  , dom = require("dom")
-  , event = require("event")
   , each = require("each")
   , map = require("map")
   , select = require("select")
   , bsnav = require("booting-sub-nav")
   , storage = require('storage')
+  
   , staticpage = require('./static')
-  , game = require('./game')
-  , game_tmpl = require('./templates/game')
-  , action_tmpl = require('./templates/index_actions')
+  , Game = require('./game')
+  , menus = require('./menu')
   
   , game_store = storage('games')
-  , game_list = game_store.get('game_list') || []
-  , correspondence_map = []
+  , game_handles = []
   ;
-
-each(game_list, function (game, index){
-  correspondence_map.push(index);
-});
 
 module.exports = {
   run: setup
@@ -38,20 +31,26 @@ function setup(){
 
 function index(ctx, next){
   var main = document.getElementById('main')
+    , game_list = game_store.get('game_list') || []
     ;
     
   main.innerHTML = '';
+  game_handles = [];
   
   if (game_list.length){
     each(game_list, function (game, index){
-      prepend(main, setGameBindings(game_tmpl({startindex: index, game: game}), index).first().els[0]);
+      var g = new Game(game);
+      g.on('change', saveGameList);
+      prepend(main, g.dom.first().els[0]);
+      game_handles.push(g);
     });
   }
   else {
     addNewGame();
   }
   
-  prepend(main, setActionBindings(action_tmpl()).first().els[0]);
+  var action_menu = new menus.ActionMenu({addNewGame: addNewGame});
+  prepend(main, action_menu.dom.first().els[0]);
 }
 
 function showLegend(ctx, next){
@@ -77,102 +76,31 @@ function append(target, content){
   target.appendChild(content);  
 }
 
-function setGameBindings(game_html, cmap_index){
-  game_html = dom(game_html);
-  cmap_index = cmap_index || 0;
-  
-  var game_root_el = game_html.first().els[0];
-  
-  game_html.find('a.save-button').each(function (el, i){
-    event.bind(el.els[0], 'click', function (){
-      game_html.first().removeClass('editing');
-      
-      var game_index = correspondence_map[cmap_index];
-      game = game_list[game_index] || {};
-      
-      //TODO: set game data
-      
-      game_list[game_index] = game;
-      saveGameList();
-    });
-  });
-  
-  game_html.find('a.process-button').each(function (el, i){
-    event.bind(el.els[0], 'click', function (){
-      //TODO
-    });
-  });
-  
-  game_html.find('a.edit-button').each(function (el, i){
-    event.bind(el.els[0], 'click', function (){
-      game_html.first().addClass('editing');
-    });
-  });
-  
-  game_html.find('a.delete-game').each(function (el, i){
-    event.bind(el.els[0], 'click', function (){
-      var game_index = correspondence_map[cmap_index];
-      
-      game_list = game_list.slice(0, game_index || 0).concat(game_list.slice(game_index + 1));
-      
-      correspondence_map = map(correspondence_map, function (item){
-        if (item !== null && item > game_index){
-          return item - 1;
-        }
-        else {
-          return item;
-        }
-      });
-      correspondence_map[cmap_index] = null;
-      
-      document.getElementById('main').removeChild(game_root_el);
-      
-      saveGameList();
-      
-      if (game_list.length === 0){
-        addNewGame();
-      }
-    });
-  });
-  
-  //TODO: input binding 
-  
-  return game_html;
-}
-
-function setActionBindings(menu_html){
-  menu_html = dom(menu_html);
-  
-  menu_html.find('a.add-game').each(function (el, i){
-    event.bind(el.els[0], 'click', addNewGame);
-  });
-  
-  
-  return menu_html;
-}
-
 function addNewGame(ev){
   if (ev){
     ev.preventDefault();
   }
   
   var main = document.getElementById('main')
-    , new_cmap_ind = correspondence_map.length
-    , new_games_index = game_list.length 
-    , new_domel = setGameBindings(game_tmpl(), new_cmap_ind).first().addClass('editing').els[0]
     ;
   
-  correspondence_map.push(new_games_index);
-  game_list.push(null);
+  var g = new Game();
+  g.dom.first().addClass('editing');
+  g.on('change', saveGameList);
+  game_handles.push(g);
     
   if (main.childNodes.length > 1){
-    main.insertBefore(new_domel, main.childNodes[1])
+    main.insertBefore(g.dom.first().els[0], main.childNodes[1])
   }
   else {
-    append(main, new_domel);
+    append(main, g.dom.first().els[0]);
   }
 }
 
 function saveGameList(){
-  game_store.set('game_list', select(game_list, function (game){ return game !== null; }));
+  var game_list = select(map(game_handles, function (game){ return game.game; }), function (game){ return game !== null; })
+  game_store.set('game_list', game_list);
+  if (game_list.length === 0){
+    addNewGame();
+  }
 }
